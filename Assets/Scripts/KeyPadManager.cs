6924a2b719ec055af4a495a58dda226ceacd33ed
Ben Carter <32357076+Ellemelmarta@users.noreply.github.com>
2018-11-07 16:27:13 +0000

    Added movement and more keypad features
    +Movement (attached to leap rig) when index and thumb are extended and others are not. NOTE: Needs testing in VR.
    
    + Fully working keypad and display for keypad (not in actual use as of yet)

---
+++ Assets/Scripts/KeyPadManager.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class KeyPadManager : MonoBehaviour {
+
+    public float timeUntilCodeReset;
+
+    public TextMeshPro numberCodeText;
+
+    public List<int> buttonNumberList;
+    private int buttonNumberListMax = 5;
+
+    private float timeSinceButtonPress = 0;
+
+    private bool loopsRunning;
+    private bool buttonPressed;
+
+
+    private void Start()
+    {
+        buttonPressed = false;
+    }
+
+
+    private void FixedUpdate()
+    {
+        if (buttonPressed != true)
+        {
+            timeSinceButtonPress += Time.deltaTime;
+        }
+
+        //if the user is to mess up this will allow the list to be reset after short amount of time
+        if (timeUntilCodeReset < timeSinceButtonPress)
+        {
+            loopsRunning = true;
+            timeSinceButtonPress = 0;
+
+            buttonNumberList.RemoveRange(0, buttonNumberList.Count);
+            numberCodeText.text = "";
+          
+            loopsRunning = false;
+        }
+    }
+
+    //Reusable function for all buttons in keypad
+    public void ButtonPressed(int buttonNumber)
+    {
+        //making sure not to edit the list while items being removed.
+        if (loopsRunning == false && buttonNumberList.Count < buttonNumberListMax)
+        {
+            buttonNumberList.Add(buttonNumber);
+            numberCodeText.text = numberCodeText.text + buttonNumber;
+        }
+
+        buttonPressed = true;
+        timeSinceButtonPress = 0;
+    }
+
+
+    public void ButtonStopPressed()
+    {
+        if (buttonPressed == true)
+        {
+            buttonPressed = false;
+        }
+    }
+}
---
+++ Assets/Scripts/VrMovement.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VrMovement : MonoBehaviour {
+
+    //Leap Rig
+    public Transform vrCamera;
+
+    public float speed = 2.0f;
+
+    private bool isActive;
+
+    //character controller is leap rig as you cannot move a vr camera you need to move parent.
+    private CharacterController cc;
+
+    private void Start()
+    {
+        cc = GetComponent<CharacterController>();
+    }
+
+    private void FixedUpdate()
+    {
+        if (isActive == true)
+        {
+            //Causes movement in the direction the headset is facing
+            Vector3 forward = vrCamera.TransformDirection(Vector3.forward);
+            cc.SimpleMove(forward * speed);
+        }
+    }
+
+    //only activates when index and thumb extended
+    public void ExtendedFingerMovementVR()
+    {
+        isActive = true;
+    }
+
+    public void ExtendedFingerMovementEnd()
+    {
+        isActive = false;
+    }
+}
