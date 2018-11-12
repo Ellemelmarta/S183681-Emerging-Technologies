609eed7ee1bd9f3b14cfe1ff91c5a12a8494d86c
Ben Carter <32357076+Ellemelmarta@users.noreply.github.com>
2018-11-12 20:41:23 +0000

    Begun UI palm feature
    +Added a debug print on palm up and fingers extended for use in future for a UI

---
+++ Assets/Scripts/LeapMotionUI.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LeapMotionUI : MonoBehaviour {
+
+    private bool palmIsUpUI;
+    private bool fingersExtendedUI;
+
+    public void OnAllFingers()
+    {
+        fingersExtendedUI = !fingersExtendedUI;
+
+        if (palmIsUpUI && fingersExtendedUI)
+        {
+            DisplayUI();
+        }
+    }
+
+    public void PalmDirectionChange()
+    {
+        palmIsUpUI = !palmIsUpUI;
+
+        if (palmIsUpUI && fingersExtendedUI)
+        {
+            DisplayUI();
+        }
+    }
+
+    private void DisplayUI()
+    {
+        print("the ui will display horray");
+    }
+
+}
